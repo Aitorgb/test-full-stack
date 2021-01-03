5dc803bad6f8fdf9c3c115ad35905246ab62270d
aitorgb <aitorguerrero9@gmail.com>
2021-01-03 16:18:58 +0100

    Feat: unfinished algorithm

--- config/routes.js
+++ config/routes.js
@@ -2,24 +2,68 @@ const express = require('express');
 const router = express.Router();
 const data = require('../bin/data.json');
 
-
 router.get('/', (req, res, next) => {
-    console.log(data)
-    res.render('config', { data })
+	res.render('config', {
+		data
+	});
 });
-// router.get('/auth/google/callback', sessionMiddleware.isNotAuthenticated, usersController.doSocialLoginGoogle);
-// router.get('/auth/slack', sessionMiddleware.isNotAuthenticated, usersController.doSocialLogin);
-// router.get('/login', sessionMiddleware.isNotAuthenticated, usersController.login);
-// router.post('/login', sessionMiddleware.isNotAuthenticated, usersController.doLogin);
-// router.get('/signup', sessionMiddleware.isNotAuthenticated, usersController.signup);
-// router.post('/users', sessionMiddleware.isNotAuthenticated, uploads.single('avatar'), usersController.createUser);
-// router.get('/activate/:token', sessionMiddleware.isNotAuthenticated, usersController.activateUser);
-// router.post('/logout', sessionMiddleware.isAuthenticated, usersController.logout);
-// router.get('/tweets', sessionMiddleware.isAuthenticated, tweetsController.list);
-// router.post('/tweets/:id/like', sessionMiddleware.isAuthenticated, tweetsController.like)
+router.post('/result', (req, res, next) => {
+	let { clients } = data;
+	const coach = req.body;
+	const allCoach = Object.keys(coach).map((name) => {
+		return {
+			name: coach[name][0],
+			valoration: parseFloat(coach[name][1]),
+			placesAvailable: parseInt(coach[name][2]),
+			clients: []
+		};
+	});
+	const clientWithMaxReputation = clients.reduce(
+		(prev, client) => (client.reputationCoach > prev.reputationCoach ? client : prev)
+	);
+	console.log(allCoach);
+	// console.log(allCoach, clients);
+	while (clients.length > 0) {
+		const clientWithMaxReputation = clients.reduce(
+			(prev, client) => (client.reputationCoach > prev.reputationCoach ? client : prev)
+		);
 
-// router.get("/", (req, res) => {
-//   res.redirect("/tweets");
-// });
+		if (clientWithMaxReputation.reputationCoach >= 7.5) {
+			const coachWithMaxReputation = allCoach.reduce(
+				(prev, coach) =>
+					coach.valoration > prev.valoration && coach.placesAvailable > coach.clients.length ? coach : prev,
+				{ valoration: 0 }
+			);
+			console.log(coachWithMaxReputation.clients.length);
+			coachWithMaxReputation.clients.push(clientWithMaxReputation);
+		} else if (clientWithMaxReputation.reputationCoach >= 5) {
+			const coachWithGoodReputation = allCoach.reduce(
+				(prev, coach) =>
+					coach.valoration > 2.5 &&
+					coach.valoration < 3.5 &&
+					coach.valoration > prev.valoration &&
+					coach.placesAvailable > coach.clients.length
+						? coach
+						: prev,
+				{ valoration: 2.5 }
+			);
+			coachWithGoodReputation.clients.push(clientWithMaxReputation);
+		} else if (clientWithMaxReputation.reputationCoach >= 0) {
+			const coachWithMinReputation = allCoach.reduce(
+				(prev, coach) =>
+					coach.valoration < prev.valoration && coach.placesAvailable > coach.clients.length ? coach : prev,
+				{ valoration: 10 }
+			);
+			coachWithMinReputation.clients.push(clientWithMaxReputation);
+		}
+		clients = clients.filter((client) => client.name !== clientWithMaxReputation.name);
+	}
+
+	allCoach.map((l) => console.log(l));
+
+	res.render('result');
+});
+
+// router.get('/auth/google/callback', sessionMiddleware.isNotAuthenticated, usersController.doSocialLoginGoogle);
 
 module.exports = router;
